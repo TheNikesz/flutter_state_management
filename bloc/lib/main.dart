01289b2ada5cc953ce49f2fdb53024ec0037a08b
30james00 <48206982+30james00@users.noreply.github.com>
2023-03-26 21:41:15 +0200

    fix: restore mocks in test

--- bloc/lib/main.dart
+++ bloc/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_app_bloc/data/data_sources/api.dart';
 import 'package:weather_app_bloc/presentation/cubits/chart_switch_cubit.dart';
 import 'package:weather_app_bloc/presentation/cubits/settings_cubit.dart';
 import 'package:weather_app_bloc/presentation/cubits/shared_preferences_cubit.dart';
@@ -27,45 +28,45 @@ class WeatherApp extends StatelessWidget {
           SharedPreferencesCubit()..getSettingsFromSharedPreferences(),
       child: BlocBuilder<SharedPreferencesCubit, SharedPreferencesState>(
           builder: (context, sharedPreferencesState) {
-            if (sharedPreferencesState is SharedPreferencesSuccess) {
-              return RepositoryProvider(
-                create: (context) => WeatherRepository(),
-                child: MultiBlocProvider(
-                  providers: [
-                    BlocProvider<SettingsCubit>(
-                      create: (context) =>
-                          SettingsCubit(
-                                  isFahrenheit: sharedPreferencesState.isFahrenheit,
-                                  isChart: sharedPreferencesState.isChart,
-                                  isNight: sharedPreferencesState.isNight),
-                    ),
-                    BlocProvider<WeatherCubit>(
-                        create: (context) => WeatherCubit(
-                                weatherRepository: context.read<WeatherRepository>(),
-                                favouriteCity: sharedPreferencesState.favouriteCity,
-                              )
-                    ),
-                    BlocProvider<ChartSwitchCubit>(
-                      create: (context) => ChartSwitchCubit(isChart: sharedPreferencesState.isChart)
-                    ),
-                    BlocProvider<WeatherSwitchCubit>(
-                      create: (context) => WeatherSwitchCubit(
-                                  isNight: sharedPreferencesState.isNight)
-                    ),
-                  ],
-                  child: MaterialApp(
-                    title: 'Weather App (Bloc)',
-                    theme: ThemeData(
-                      textTheme: GoogleFonts.montserratTextTheme(),
-                      scaffoldBackgroundColor: Colors.white,
-                    ),
-                    home: const WeatherPage(),
-                  ),
+        if (sharedPreferencesState is SharedPreferencesSuccess) {
+          return RepositoryProvider(
+            create: (context) => WeatherRepository(
+              geocodingApi: GeocodingApiMock(),
+              weatherForecastApi: WeatherForecastApiMock(),
+            ),
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider<SettingsCubit>(
+                  create: (context) => SettingsCubit(
+                      isFahrenheit: sharedPreferencesState.isFahrenheit,
+                      isChart: sharedPreferencesState.isChart,
+                      isNight: sharedPreferencesState.isNight),
                 ),
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }        
+                BlocProvider<WeatherCubit>(
+                    create: (context) => WeatherCubit(
+                          weatherRepository: context.read<WeatherRepository>(),
+                          favouriteCity: sharedPreferencesState.favouriteCity,
+                        )),
+                BlocProvider<ChartSwitchCubit>(
+                    create: (context) => ChartSwitchCubit(
+                        isChart: sharedPreferencesState.isChart)),
+                BlocProvider<WeatherSwitchCubit>(
+                    create: (context) => WeatherSwitchCubit(
+                        isNight: sharedPreferencesState.isNight)),
+              ],
+              child: MaterialApp(
+                title: 'Weather App (Bloc)',
+                theme: ThemeData(
+                  textTheme: GoogleFonts.montserratTextTheme(),
+                  scaffoldBackgroundColor: Colors.white,
+                ),
+                home: const WeatherPage(),
+              ),
+            ),
+          );
+        } else {
+          return const CircularProgressIndicator();
+        }
       }),
     );
   }
