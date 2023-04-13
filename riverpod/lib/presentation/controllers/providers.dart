621bc3591f839d6d21fe41c97e4012dba6bcabd1
30james00 <48206982+30james00@users.noreply.github.com>
2023-04-13 11:39:35 +0200

    fix: riverpod mocks

--- riverpod/lib/presentation/controllers/providers.dart
+++ riverpod/lib/presentation/controllers/providers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather_app_riverpod/data/data_sources/api.dart';
 import 'package:weather_app_riverpod/presentation/controllers/states/shared_preferences_state.dart';
 
 import '../../data/repositories/weather_repository.dart';
@@ -7,35 +8,40 @@ import '../../domain/models/weather.dart';
 import 'states/settings_state.dart';
 
 final weatherRepositoryProvider = Provider<WeatherRepository>((ref) {
-  return WeatherRepository();
+  return WeatherRepository(
+      geocodingApi: GeocodingApiMock(),
+      weatherForecastApi: WeatherForecastApiMock());
 });
 
 final cityProvider = StateProvider<String>((ref) {
   return 'Warsaw';
 });
 
-final weatherProvider = FutureProvider.autoDispose.family<List<Weather>, String>((ref, city) {
+final weatherProvider =
+    FutureProvider.autoDispose.family<List<Weather>, String>((ref, city) {
   final weatherRepository = ref.watch(weatherRepositoryProvider);
   return weatherRepository.getWeeklyForecast(city);
 });
 
 final settingsProvider = StateProvider<SettingsState>((ref) {
-  return const SettingsState(isFahrenheit: false, isNight: false, isChart: false);
+  return const SettingsState(
+      isFahrenheit: false, isNight: false, isChart: false);
 });
 
-final sharedPrefenecesProvider = FutureProvider.autoDispose<SharedPreferencesState>((ref) async {
+final sharedPrefenecesProvider =
+    FutureProvider.autoDispose<SharedPreferencesState>((ref) async {
   final sharedPreferences = await SharedPreferences.getInstance();
   final bool? isFahrenheit = sharedPreferences.getBool('isFahrenheit');
   final bool? isChart = sharedPreferences.getBool('isChart');
   final bool? isNight = sharedPreferences.getBool('isNight');
   final String? favouriteCity = sharedPreferences.getString('favouriteCity');
-  
+
   return SharedPreferencesState(
     isFahrenheit: isFahrenheit ?? false,
     isChart: isChart ?? false,
     isNight: isNight ?? false,
     favouriteCity: favouriteCity ?? 'Warsaw',
-    );
+  );
 });
 
 final weatherSwitchProvider = StateProvider<bool>((ref) {
