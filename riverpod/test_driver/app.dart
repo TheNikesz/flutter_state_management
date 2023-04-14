27c32d625390d76ad0f0522bef81e1f643835263
30james00 <48206982+30james00@users.noreply.github.com>
2023-04-14 15:07:16 +0200

    fix: remove old frametime profiling

--- riverpod/test_driver/app.dart
+++ riverpod/test_driver/app.dart
@@ -1,15 +1,7 @@
 import 'package:weather_app_riverpod/main.dart' as app;
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_driver/driver_extension.dart';
 
 void main() {
-  late SchedulerBinding schedulerBinding;
   enableFlutterDriverExtension();
   app.main();
-  schedulerBinding = SchedulerBinding.instance;
-  schedulerBinding.addTimingsCallback((timings) {
-    for (final timing in timings) {
-      print('${timing.frameNumber}, ${timing.totalSpan.inMicroseconds};');
-    }
-  });
 }
